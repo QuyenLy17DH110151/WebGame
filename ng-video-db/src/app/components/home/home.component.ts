a61518e111af013f8193ec63e07cbf7587c43845
21AK22-COM\Administrator <lqboss1020304050@gmail.com>
2022-03-20 16:58:02 +0700

    implement home

--- ng-video-db/src/app/components/home/home.component.ts
+++ ng-video-db/src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { tap } from 'rxjs';
+import { APIResponse } from 'src/app/models/apiResponse.model';
+import { Game } from 'src/app/models/game.model';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public sort: string = '';
-  constructor() {}
+  public games: Array<Game> = new Array<Game>();
+  constructor(
+    private activedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activedRoute.params
+      .pipe(
+        tap((params: Params) => {
+          if (params['game-search']) {
+            this.searchGames('metacrit', params['game-search']);
+          } else {
+            this.searchGames('metacrit');
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpService
+      .getGameList(sort, search)
+      .pipe(
+        tap((games: APIResponse<Game>) => {
+          this.games = games.results;
+        })
+      )
+      .subscribe();
+  }
 }
