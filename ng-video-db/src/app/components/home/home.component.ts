3f7e8a2ed1193224c3030a816794606d64d12164
21AK22-COM\Administrator <lqboss1020304050@gmail.com>
2022-03-20 17:07:08 +0700

    destroy home

--- ng-video-db/src/app/components/home/home.component.ts
+++ ng-video-db/src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { tap } from 'rxjs';
+import { Subscription, tap } from 'rxjs';
 import { APIResponse } from 'src/app/models/apiResponse.model';
 import { Game } from 'src/app/models/game.model';
 import { HttpService } from 'src/app/services/http.service';
@@ -10,36 +10,46 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort: string = '';
   public games: Array<Game> = new Array<Game>();
+  private subs = new Subscription();
+
   constructor(
     private activedRoute: ActivatedRoute,
     private httpService: HttpService
   ) {}
 
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
   ngOnInit(): void {
-    this.activedRoute.params
-      .pipe(
-        tap((params: Params) => {
-          if (params['game-search']) {
-            this.searchGames('metacrit', params['game-search']);
-          } else {
-            this.searchGames('metacrit');
-          }
-        })
-      )
-      .subscribe();
+    this.subs.add(
+      this.activedRoute.params
+        .pipe(
+          tap((params: Params) => {
+            if (params['game-search']) {
+              this.searchGames('metacrit', params['game-search']);
+            } else {
+              this.searchGames('metacrit');
+            }
+          })
+        )
+        .subscribe()
+    );
   }
 
   searchGames(sort: string, search?: string): void {
-    this.httpService
-      .getGameList(sort, search)
-      .pipe(
-        tap((games: APIResponse<Game>) => {
-          this.games = games.results;
-        })
-      )
-      .subscribe();
+    this.subs.add(
+      this.httpService
+        .getGameList(sort, search)
+        .pipe(
+          tap((games: APIResponse<Game>) => {
+            this.games = games.results;
+          })
+        )
+        .subscribe()
+    );
   }
 }
